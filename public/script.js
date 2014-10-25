b6ee8217db4662c0a29c0225da3b09b206c1c5ea
Patricio Sanchez <partricios94@gmail.com>
2014-10-24 23:56:00 -0500

    HOPE

--- public/script.js
+++ public/script.js
@@ -2,7 +2,6 @@ SC.initialize({
   client_id: "67df025394fb58a59495a2b8f8573ddd"
 });
 
-
 $(document).ready(function() {
   SC.get('/tracks', { genres: 'Rock' }, function(tracks) {
     $(tracks).each(function(index, track) {
