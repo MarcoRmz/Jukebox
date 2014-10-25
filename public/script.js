19ae6bd0b626b757295aea8708a3c3f1e85fd928
Patricio Sanchez <partricios94@gmail.com>
2014-10-24 23:18:51 -0500

    lost in translation

---
+++ public/script.js
@@ -0,0 +1,12 @@
+SC.initialize({
+  client_id: "67df025394fb58a59495a2b8f8573ddd"
+});
+
+
+$(document).ready(function() {
+  SC.get('/tracks', { genres: 'Rock' }, function(tracks) {
+    $(tracks).each(function(index, track) {
+      $('#soundcloud-div').append($('<li></li>').html(track.title + ' - ' + track.genre));
+    });
+  });
+});
